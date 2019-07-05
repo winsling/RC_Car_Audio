bef6525f6c8c778c40cda2331ea1cc9e84dc62de
winsling <winsling@yahoo.de>
2019-07-05 14:51:16 +0200

    delete obsolete files

--- src/Audio.ino
+++ src/Audio.ino
@@ -11,12 +11,29 @@ int old_ACW=0;
 
 int AudioCommandWord=0;
 
+struct MultiBtnBitFieldType {
+  bool MultiBlueBtn:1;
+  bool MultiWhiteBtn:1;
+  bool MultiYellowBtn:1;
+  bool MultiRedBtn:1;
+  bool MultiOrangeBtn:1;
+  bool MultiGreenBtn:1;
+  bool MultiGrayBtn:1;
+  bool MultiBlackBtn:1;
+};
+
+union MultiBtnCharType {
+
+  MultiBtnBitFieldType MultiBtnBitField;
+  char MultiBtnByte;
+} MultiBtnRcvChar;
 
 void receiveEvent(int CommandWord)
 {
   while(Wire.available()){
     CommandWord = Wire.read();
     AudioCommandWord = CommandWord;
+    MultiBtnRcvChar.MultiBtnByte = (char) CommandWord;
     Serial.println(CommandWord);
   }	
 }
@@ -48,10 +65,11 @@ tmrpcm.volume(1);
 void loop() { 
 
 
- if ((AudioCommandWord == 1) && (AudioCommandWord!=old_ACW)) {
-  Serial.print("Playing:");
-  
-  tmrpcm.play("gt40.wav");
+ if ((AudioCommandWord > 0) && (AudioCommandWord!=old_ACW)) {
+  if (MultiBtnRcvChar.MultiBtnBitField.MultiBlackBtn) {
+    Serial.print("Playing:");
+    tmrpcm.play("gt40.wav");
+    }
   }
   old_ACW=AudioCommandWord; 
   return;   // don't do anything more if not
